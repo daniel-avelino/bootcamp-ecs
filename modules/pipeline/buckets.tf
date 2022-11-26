ebe3f95036cae3c9b17573fd52b527b04fb8a753
daniel-avelino <74691409+daniel-avelino@users.noreply.github.com>
2022-11-26 14:39:57 -0300

    ajustes

--- modules/pipeline/buckets.tf
+++ modules/pipeline/buckets.tf
@@ -1,5 +1,9 @@
 resource "aws_s3_bucket" "source" {
   bucket            = "${var.cluster_name}-${var.account_id}-pipeline"
-  aws_s3_bucket_acl = "private"
   force_destroy     = true
+}
+
+resource "aws_s3_bucket_acl" "example_bucket_acl" {
+  bucket = aws_s3_bucket.source.id
+  acl    = "private"
 }
