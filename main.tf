beedbbcc91338f75b8b278b1cd5c64fa124ae8f0
daniel-avelino <74691409+daniel-avelino@users.noreply.github.com>
2022-11-26 17:09:33 -0300

    ajustes

--- main.tf
+++ main.tf
@@ -8,14 +8,10 @@ terraform {
 }
 
 provider "aws" {
-  profile = "default"
   region = "${var.aws_region}"
-
   default_tags {
     tags = {
       Environment = "BootCamp"
     }
   }
 }
-
-data "aws_caller_identity" "current" {}
